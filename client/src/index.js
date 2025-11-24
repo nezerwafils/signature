9ca65543f977c53e67506dab563201cf55a7e972
nezerwafils <122962699+nezerwafils@users.noreply.github.com>
2025-11-24 15:59:01 +0000

    Fix React 19 compatibility: use createRoot instead of ReactDOM.render

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
